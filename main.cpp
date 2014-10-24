32514ce1f6d737c806a1e90c6d0dff6592cb16eb
Sitnikova <turumbayka@bk.ru>
2014-10-24 10:08:44 +0400

    Update main.cpp: Add enter numbers.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
-#include "std.h"
+#include "sdt.h"
 int main()
 {
+    int a;
+    cout << "Enter first number ";
+    cin >> a;
+    int b;
+    cout << "\n" << "Enter second number ";
+    cin >> b;
 
 }
